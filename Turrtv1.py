65ba831ae0f0cdfaf31898cc4588322381c98f3a
dylancor2 <137826466+dylancor2@users.noreply.github.com>
2024-06-09 14:50:43 -0400

    Add files via upload

---
+++ Turrtv1.py
@@ -0,0 +1,40 @@
+import keyboard
+import serial
+import time
+
+arduino = serial.Serial(port = 'COM3', baudrate=9600, timeout = 0.1)
+
+x = 0
+y = 0
+def incrementY():
+    arduino.write(b'1')
+    time.sleep(0.1)
+    data = arduino.readline()
+    print(data)
+
+def decrementY():
+    arduino.write(b'2')
+    time.sleep(0.1)
+    data = arduino.readline()
+    print(data)
+
+def incrementX():
+    arduino.write(b'3')
+    time.sleep(0.1)
+    data = arduino.readline()
+    print(data)
+
+def decrementx():
+    arduino.write(b'4')
+    time.sleep(0.1)
+    data = arduino.readline()
+    print(data)
+
+keyboard.add_hotkey('up', lambda: incrementY())
+keyboard.add_hotkey('down', lambda: decrementY())
+
+keyboard.add_hotkey('left', lambda: incrementX())
+keyboard.add_hotkey('right', lambda: decrementx())
+keyboard.wait('esc')
+print('Exiting...')
+arduino.write(b'5')
